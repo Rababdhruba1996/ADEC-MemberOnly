4e63ceff5f7d645693fd0b01032ffb7c88d9e816
Rabab <rshayra@uxd.co.jp>
2023-03-28 10:34:23 +0900

    fix access problem and consent scroll

--- wp_5rokkgvod5/wp-content/plugins/logged-in-access/logged-in-access.php
+++ wp_5rokkgvod5/wp-content/plugins/logged-in-access/logged-in-access.php
@@ -7,8 +7,10 @@
  */
 
 function logged_in_access_control() {
+    // Get the current page ID
+    $page_id = get_queried_object_id();
     // Check if the user is not logged in and not on the home page
-    if (!is_user_logged_in() && !is_home() && !is_front_page()) {
+    if (!is_user_logged_in() && !is_home() && !is_front_page() && !is_page('会員登録') && !is_page('パスワードのリセット') && wp_get_post_parent_id($page_id) !== get_page_by_path('会員登録')->ID && wp_get_post_parent_id($page_id) !== get_page_by_path('パスワードのリセット')->ID) {
         // Redirect to the home page or any other desired URL
         wp_redirect(home_url());
         exit;
